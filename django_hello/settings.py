1f801c0ae47747f8cef77f363d21c3c0b79c553b
Yang minqiang <obsicn@hotmail.com>
2017-03-31 16:58:02 +0800

    modify settings.py for the integration between mod_wsgi

--- django_hello/settings.py
+++ django_hello/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'hello_app.apps.HelloAppConfig',
+    'mod_wsgi.server',
 ]
 
 MIDDLEWARE = [
--- hello_app/views.py
+++ hello_app/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import socket
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse('Hello Django')
+    local_hostname = socket.gethostname()
+    return HttpResponse('Hello Django ' + local_hostname)
